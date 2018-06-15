e9c8740a76bbf067ea304ca6514dea0dc6ca7e07
Russell Feldhausen <russfeldh@gmail.com>
2018-06-15 09:48:29 -0500

    Initial Commit

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index(){
+      	return response()->json("It works", 200);
+    }
+
+    public function store(Request $request){
+      $tag = $request->input('tag');
+      if(is_null($tag) || strlen($tag) == 0){
+        return response()->json("Tag not sent", 404);
+      }
+      $value = $request->input('value');
+      if(is_null($value)){
+        $value = "";
+      }
+      $current = app('db')->table('data')->where('key', $tag)->limit(1)->value('value');
+      if(is_null($current)){
+        app('db')->table('data')->insert(['key' => $key, 'value' => $value]);
+      }else{
+        app('db')->table('data')->where('key', $tag)->update(['value' => $value]);
+      }
+      $return = array("STORED", $tag, $value);
+      return response()->json($return, 200);
+    }
+
+    public function get(Request $request){
+      $tag = $request->input('tag');
+      if(is_null($tag) || strlen($tag) == 0){
+        return response()->json("Tag not sent", 404);
+      }
+      $value = app('db')->table('data')->where('key', $tag)->limit(1)->value('value');
+      if(is_null($value)){
+        $value = "";
+      }
+      $return = array("VALUE", $tag, $value);
+      return response()->json($return, 200);
+    }
+
+    //
+}
